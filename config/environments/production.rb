81ebca119d066f0369e64b844557284c457b607f
igor-starostenko <contactigorstar@gmail.com>
2017-08-20 14:54:20 -0700

    fixed confirmation routes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,6 +93,8 @@ Rails.application.configure do
 
   # Sendgrid mail configuration
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'https://photo-app-igorstar.herokuapp.com',
-                                               protocol: 'https' }
+  config.action_mailer.default_url_options = {
+    host: 'https://photo-app-igorstar.herokuapp.com',
+    protocol: 'https'
+  }
 end
