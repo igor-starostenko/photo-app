ff9df4ea311b046d3845589ccb9d687903df613f
igor-starostenko <contactigorstar@gmail.com>
2017-09-01 23:34:20 -0700

    fixed InvalidAuthenticityToken error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,6 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session,
+    only: Proc.new { |c| c.request.format.json? }
 end
